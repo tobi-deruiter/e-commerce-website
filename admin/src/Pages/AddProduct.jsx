2e48f2a9e682351825c098ceb3492657ced08f62
tobi-deruiter <zachderuiter@gmail.com>
2025-03-03 16:32:14 -0500

    container for add product form to make sure its centered

--- admin/src/Pages/AddProduct.jsx
+++ admin/src/Pages/AddProduct.jsx
@@ -1,9 +1,17 @@
 import React from "react"
 import AddProductForm from "../Components/AddProductForm";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+`
 
 const AddProduct = (props) => {
     return (
-        <AddProductForm />
+        <Container>
+            <AddProductForm />
+        </Container>
     )
 };
 
